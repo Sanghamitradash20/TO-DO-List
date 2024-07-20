d592b96bff94e496a4f3a7741d364907ebe337eb
Sanghamitradash20 <dashsanghamitra444@gmail.com>
2024-07-21 00:46:24 +0530

    changes in database

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/todolist")
+// mongoose.connect("mongodb://localhost:27017/todolist")
+mongoose.connect("mongodb+srv://dashsanghamitra444:iKIZ26U3XD4MBydk@cluster0.wooxjr5.mongodb.net/todolist")
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Could not connect to MongoDB", err));
 
